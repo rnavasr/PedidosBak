15454cfd3da377dc32fdac19cf9c3cf68025dcd4
rnavasr <rafaelnavas1967@gmail.com>
2024-01-15 17:13:58 -0500

    espacio bodega

--- Bodega/urls.py
+++ Bodega/urls.py
@@ -4,5 +4,6 @@ from .views import *
 urlpatterns = [
     path('crear/', CrearBodegaView.as_view(), name='crear_bodega'),
     path('listar/', ListarBodegasView.as_view(), name='listar_bodegas'),
+    
     path('editar/<int:bodega_id>/', EditarBodegaView.as_view(), name='editar_bodega')
 ]
